aef1252a25e153728c70de4789037f38e80b0eba
JoshEdgell <josh.0.edgell@gmail.com>
2017-09-20 14:06:07 -0400

    made some changes?

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created
+      render json: @user.to_json(include: [:badges]), status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
-      render json: @user
+      render json: @user.to_json(include: [:badges])
     else
       render json: @user.errors, status: :unprocessable_entity
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,38 +8,38 @@
 
 
 Achievement.create([
-  { name: 'Animal Geek' },
+  { name: 'Animal Geek' },    #1
 { name: 'Animal Wonk' },
 { name: 'Animal Nerd' },
 { name: 'Book Geek' },
-{ name: 'Book Wonk' },
+{ name: 'Book Wonk' },        #5
 { name: 'Book Nerd' },
 { name: 'Computer Geek' },
 { name: 'Computer Wonk' },
 { name: 'Computer Nerd' },
-{ name: 'Video Game Geek' },
+{ name: 'Video Game Geek' },  #10
 { name: 'Video Game Wonk' },
 { name: 'Video Game Nerd' },
 { name: 'History Geek' },
 { name: 'History Wonk' },
-{ name: 'History Nerd' },
+{ name: 'History Nerd' },     #15
 { name: 'Movie Geek' },
 { name: 'Movie Wonk' },
 { name: 'Movie Nerd' },
 { name: 'Music Geek' },
-{ name: 'Music Wonk' },
+{ name: 'Music Wonk' },       #20
 { name: 'Music Nerd' },
 { name: 'Science Geek' },
 { name: 'Science Wonk' },
 { name: 'Science Nerd' },
-{ name: 'Sports Geek' },
+{ name: 'Sports Geek' },      #25
 { name: 'Sports Wonk' },
 { name: 'Sports Nerd' },
 { name: 'Television Geek' },
 { name: 'Television Wonk' },
-{ name: 'Television Nerd' },
-{ name: 'Trivia Geek' },
-{ name: 'Trivia Wonk' },
-{ name: 'Trivia Nerd' },
-{ name: 'Trivia Legend' }
+{ name: 'Television Nerd' },  #30
+{ name: 'Trivia Geek' },      #Earned after 5 badges
+{ name: 'Trivia Wonk' },      #Earned after 12 badges
+{ name: 'Trivia Nerd' },      #Earned after 20 badges
+{ name: 'Trivia Legend' }     #Earned after 30 badges
   ])
