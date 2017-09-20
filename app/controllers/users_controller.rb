e15f0b25838093639f67a6a05d0a55e3026097f1
JoshEdgell <josh.0.edgell@gmail.com>
2017-09-19 22:00:08 -0400

    corrected routes - data is sending correctly now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,12 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
-    render json: @users.to_json(include: :badges)
+    render json: @users.to_json(include: [:badges])
   end
 
   # GET /users/1
   def show
-    render json: @user.to_json(include: :badges)
+    render json: @user.to_json(include: [:badges])
   end
 
   #LOGIN
